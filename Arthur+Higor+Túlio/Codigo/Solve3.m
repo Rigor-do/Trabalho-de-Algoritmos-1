a3820f21457924df27098a88b351a84a7754503b
Higor David O <43549329+hdogithub@users.noreply.github.com>
2020-11-24 00:32:50 -0300

    Alteração final de arquivos e pastas antes da subsmissão
    Colocado os códigos da submissão em "Código", o relatório em "Documentação" e as outras funções em "Outros códigos". Comentado o código da solução da questão 3.
    
    Co-Authored-By: Tulio Brunoro <21254205+mactep@users.noreply.github.com>

--- Arthur+Higor+Túlio/Codigo/Solve3.m
+++ Arthur+Higor+Túlio/Codigo/Solve3.m
@@ -1,19 +1,20 @@
-t0 = 0;
-v0 = 500;
-h = 1;
-n = 100;
+v0 = 500; t0 = 0; % Declaração do PVI
+h = 1; % Passo da discretização
+n = 100; % Número de passos
 
-function retorno = f(t)
+function retorno = f(t) % Declaração da função que calcula a parte da variação do fluxo
   retorno = influx(t) - outflux(t) - vazamento(t);
 end
 
-T = zeros(1, n);
+% Pré-alocações dos vetores com os dados
 V = zeros(1, n);
+T = zeros(1, n);
 influxs = zeros(1, n);
 outfluxs = zeros(1, n);
 vazamentos = zeros(1, n);
 etas = zeros(1, n);
 
+% Definição dos primeiros termos dos vetores
 t = t0; v = v0;
 T(1) = t; V(1) = v;
 influxs(1) = influx(1);
@@ -22,6 +23,7 @@ vazamentos(1) = vazamento(1);
 etas(1) = rand() * 6 - 3;
 Q(1) = f(1);
 
+% Calculo de todos os pontos discretos
 for i=2:n
   eta = rand() * 6 - 3;
   etas(i) = eta;
@@ -35,12 +37,15 @@ for i=2:n
   Q(i) = f(i);
 end
 
+% Print na forma dada de tabela
 printTabXY( T, 'T', V, 'V', '%.5e', 'Euler' );
 
+% Abre uma nova janela para o plot
 figure('name', 'Gilberto');
 
 hold on;
 legends = {};
+% Plots
 plot(T, V-v0, "k-");
 legends(1) = "V-V0";
 plot(T, influxs, "g-");
@@ -54,6 +59,7 @@ legends(5) = "ruidos";
 plot(T, Q, "b-");
 legends(6) = "Q";
 
+% Aplicação das legendas
 xlabel("t[h]");
 ylabel("Fluxo [Q(m^3/h)] ----- Volume [m^3]");
 legend(legends, "location", "northeast");
